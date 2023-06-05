7c432ffeb124dbe473f5b203ac62f0bc135c512c
Leticia Evelin <ljkeleticia@gmail.com>
2023-06-05 17:43:14 -0300

    validacao ok, falta estilizar

--- js/loginAdm.js
+++ js/loginAdm.js
@@ -1,12 +1,71 @@
  
- const form = document.querySelector(".validation");
+ const form = document.querySelector(".dashboard");
  const email = document.getElementById("input-email");
  const senha = document.getElementById("input-senha");
+ const message = document.getElementById("descricao-input");
 
 const button = document.getElementById('input-enviar');
 
+function setErrorFor(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector(".error-message");
+
+    // Adiciona a mensagem de erro
+    small.textContent = message;
+
+    // Adiciona a classe de erro
+    formControl.classList.remove("success");
+    formControl.classList.add("error");
+}
+
+function setSuccessFor(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector(".success-message");
+
+    small.textContent = message;
+  
+    // Adicionar a classe de sucesso
+    formControl.classList.remove("error");
+    formControl.classList.add("success");
+}
+
+function checkEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
+
+function checkInputs() {
+
+    if (email.value === "") {
+        setErrorFor(email, "O e-mail é obrigatória!");
+    } else if (!checkEmail(email.value)) {
+        setErrorFor(email, "Por favor, insira um email válido!");
+    } else {
+        setSuccessFor(email);
+    }
+
+    if (senha.value === "") {
+        setErrorFor(senha, "A senha é obrigatória.");
+    } else {
+        setSuccessFor(senha);
+    }
+   
+
+    const formControls = form.querySelectorAll(".form-group");
+
+    const formIsValid = [...formControls].every((formControl) => {
+        return formControl.classList.contains("success");
+    });
+
+    if (formIsValid) {
+        console.log("O formulário está 100% válido!");
+    }
+   
+}  
+
 button.addEventListener("click", async (e) => {
   e.preventDefault();
+  checkInputs();
+
 
   const administrador = {   
     "id": "",
@@ -31,7 +90,13 @@ button.addEventListener("click", async (e) => {
       window.location.href = '../dash_cadastro/index.html';
     } else {
       // Acesso negado, exibir mensagem de erro
-      console.log(data.message);
+      if (response.status === 401) {
+        setErrorFor(senha, "Email ou senha incorretos.");
+      } else {
+        // Outro erro
+        console.log(data.message);
+    }
+      
     }
   
 });
