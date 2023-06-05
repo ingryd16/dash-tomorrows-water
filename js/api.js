5c400bce83170f9c8fceff6538f5883a24ec437e
INGRYD SHIRLLEY DA SILVA RIBEIRO <22282092@CFP127ALUNO.LOCAL>
2023-06-05 09:25:19 -0300

    oi

--- js/api.js
+++ js/api.js
@@ -2,7 +2,6 @@
 
 export const produtos = async () => {
    
-  
     const url = `http://localhost:8080/v1/tomorrows-water/produto`
     const response = await fetch(url)
     const data = await response.json()
@@ -12,5 +11,17 @@ export const produtos = async () => {
         ...data
     }
     
+}
+
+export const projetos = async () => {
+   
+    const url = `http://localhost:8080/v1/tomorrows-water/projeto`
+    const response = await fetch(url)
+    const data = await response.json()
+    const { projetos } = data;
+    console.log(projetos)
+    return {
+        ...data
+    }
     
 }
---
+++ js/carregarProjetos.js
@@ -0,0 +1,42 @@
+'use strict'
+
+import { projetos } from "./api.js";
+let dados = await projetos();
+console.log(dados)
+
+const cardProjeto = (dados) => {
+
+    const card = document.createElement('div')
+    card.classList.add('card')
+
+
+    const text_container = document.createElement('div')
+    card.classList.add('text_container')
+
+    const name = document.createElement('h2')
+    name.classList.add('card__name')
+    name.textContent = dados.nome;
+
+    const description = document.createElement('p')
+    description.classList.add('card__biography')
+    description.textContent = dados.descricao;
+
+    text_container.append(name /* description */)
+
+    const img = document.createElement('img')
+    img.classList.add('card__img')
+    img.src = dados.imagem;
+
+    card.append(img, text_container, description)
+
+    return card
+}
+
+export const carregarProjeto = async () => {
+
+    const container = document.getElementById('container-collections')
+    const cards = dados.projetos.map(cardProjeto)
+    container.append(...cards)
+}
+
+cardProjeto()
